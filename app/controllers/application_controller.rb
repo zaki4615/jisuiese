a4493a108fca13c36d00d8f646b540e9e8a4ded7
Wataru Kanzaki <224184043+zaki4615@users.noreply.github.com>
2025-12-10 03:55:58 +0000

    Fix logout 404 error: add after_sign_out_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,11 @@ class ApplicationController < ActionController::Base
     my_page_path
   end
 
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
