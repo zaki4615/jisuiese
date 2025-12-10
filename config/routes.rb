21e97328f6e2f5ff30692f0a6de7c36c8639e75d
Wataru Kanzaki <224184043+zaki4615@users.noreply.github.com>
2025-12-10 02:56:14 +0000

    検索機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
+  get 'search/search'
   devise_for :users
   root to: 'homes#top'
   get "home/about" => "homes#about", as: 'about'
   get "users/my_page" => "users#my_page", as: "my_page"
   resources :recipes
   resources :users, only: [:show, :edit, :update, :destroy]
+  get "search", to: "search#search"
 end
